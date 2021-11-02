da7ddd3c6b888b30fda2f0e8caf0814e518435de
Subhadeep <subhadeep0326@gmail.com>
2021-11-02 22:38:34 +0530

    subhadeep added the remove 'avoidable' change

--- HashTables_BST_Day15/Program.cs
+++ HashTables_BST_Day15/Program.cs
@@ -21,6 +21,9 @@ namespace HashTables_BST_Day15
             }
             Console.WriteLine("Displaying after add operation\n");
             hashtable.Display();
+            hashtable.Remove("avoidable");                                     //display after removing the key value pair with key avoidable
+            Console.WriteLine("Displaying after removing avoidable.\n");
+            hashtable.Display();
             Console.WriteLine("----------------------------------------");
         }
         static void Main(string[] args)
